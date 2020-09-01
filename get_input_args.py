2e12e879fdc293f9b3ef4b70d7b457a1dd3ec037
Ronald Yonggi <ronaldyonggi@gmail.com>
2020-09-01 15:52:49 -0400

    added name and date created info

--- get_input_args.py
+++ get_input_args.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                   
+# PROGRAMMER: Ronald Yonggi
+# DATE CREATED: September 1st, 2020                       
 # REVISED DATE: 
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
