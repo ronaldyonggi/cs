6a65dbf5c79a0c531cfec3ae7398066e9a51c22c
Ronald Yonggi <ronaldyonggi@gmail.com>
2020-09-01 17:09:21 -0400

    added name and date created info

--- get_pet_labels.py
+++ get_pet_labels.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
+# PROGRAMMER: Ronald Yonggi
+# DATE CREATED: September 1st, 2020                      
 # REVISED DATE: 
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
