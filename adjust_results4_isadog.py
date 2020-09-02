1be3271fc9a5b852191f4e80b6312173af587c9a
Ronald Yonggi <ronaldyonggi@gmail.com>
2020-09-02 11:44:17 -0400

    added name, date created info

--- adjust_results4_isadog.py
+++ adjust_results4_isadog.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/adjust_results4_isadog.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                 
+# PROGRAMMER: Ronald Yonggi
+# DATE CREATED: September 2nd, 2020                                 
 # REVISED DATE: 
 # PURPOSE: Create a function adjust_results4_isadog that adjusts the results 
 #          dictionary to indicate whether or not the pet image label is of-a-dog, 
