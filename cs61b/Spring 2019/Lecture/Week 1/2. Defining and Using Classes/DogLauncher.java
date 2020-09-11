a3c0582990fb767baf804b3d0c9a26f4a0c0ed36
Ronald Yonggi <ronaldyonggi@gmail.com>
2020-09-11 17:28:13 -0400

    moved cs61b to ucberkeley directory

--- cs61b/Spring 2019/Lecture/Week 1/2. Defining and Using Classes/DogLauncher.java
+++
@@ -1,7 +0,0 @@
-public class DogLauncher{
-    /* DogLauncher has a main method! */
-    public static void main(String[] args){
-        /* We want to call the makeNoise method of the Dog class */
-        Dog.makeNoise();
-    }
-}
