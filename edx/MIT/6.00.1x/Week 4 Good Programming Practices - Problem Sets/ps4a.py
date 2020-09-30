e500b96cfbef63b709772bcd3ed171d81621499c
Ronald Yonggi <ronaldyonggi@gmail.com>
2020-09-30 14:37:03 -0400

    completed calculateHandlen implementation

--- edx/MIT/6.00.1x/Week 4 Good Programming Practices - Problem Sets/ps4a.py
+++ edx/MIT/6.00.1x/Week 4 Good Programming Practices - Problem Sets/ps4a.py
@@ -184,8 +184,6 @@ def isValidWord(word, hand, wordList):
     # Otherwise, the word is valid.
     return True
 
-
-
 #
 # Problem #4: Playing a hand
 #
@@ -197,7 +195,7 @@ def calculateHandlen(hand):
     hand: dictionary (string-> int)
     returns: integer
     """
-    # TO DO... <-- Remove this comment when you code this function
+    return sum(hand.values())
 
 
 
