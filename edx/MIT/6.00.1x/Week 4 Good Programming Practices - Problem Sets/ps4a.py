1fbdfb20b58048d7ef32fc445ea0362da1f3bfc1
Ronald Yonggi <ronaldyonggi@gmail.com>
2020-09-30 14:16:07 -0400

    implemented updateHand function

--- edx/MIT/6.00.1x/Week 4 Good Programming Practices - Problem Sets/ps4a.py
+++ edx/MIT/6.00.1x/Week 4 Good Programming Practices - Problem Sets/ps4a.py
@@ -144,9 +144,10 @@ def updateHand(hand, word):
     hand: dictionary (string -> int)    
     returns: dictionary (string -> int)
     """
-    # TO DO ... <-- Remove this comment when you code this function
-
-
+    copy = hand.copy()
+    for letter in word:
+        copy[letter] -= 1
+    return copy
 
 #
 # Problem #3: Test word validity
