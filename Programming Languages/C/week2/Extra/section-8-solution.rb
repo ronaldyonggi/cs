c25d01cf32f1ce25007e854142a7d189f0c4bef1
Ronald Yonggi <ronaldyonggi@gmail.com>
2021-01-10 09:34:20 -0500

    added problem statement

--- Programming Languages/C/week2/Extra/section-8-solution.rb
+++ Programming Languages/C/week2/Extra/section-8-solution.rb
@@ -1,3 +1,16 @@
+# You're given an ML program implementing a small bit of a very simple role-playing game.
+# This is not an actual game, just something that pits a character (either a knight or 
+# a wizard) against a series of various challenges. Since knights and wizards have very
+# different approaches to solving the problems facing them, we need to implement small pieces
+# of logic changing the state of the game depending on the combination of character's type
+# and the type of the challenge. Looks like a perfect match for double dispatch!
+
+# Your task is to inspect the provided ML definitions and reimplement the same logic in Ruby
+# using a principled object-oriented approach. A template file for solution is also provided,
+# as well as some code responsible for progressing the overall world state.
+
+# Note that the double dynamic dispatch is asymmetric in this case.
+
 require_relative './section-8-provided'
 
 class Character
